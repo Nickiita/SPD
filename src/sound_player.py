06660d30a3ef6b4bee7a70ea627b1e55bc0de6c0
Nickii <51742504+Nickiita@users.noreply.github.com>
2024-06-14 00:25:22 +1000

    Update sound_player.py

--- src/sound_player.py
+++ src/sound_player.py
@@ -1,11 +1,23 @@
 import sounddevice as sd
 import soundfile as sf
+import threading
 
 
 class SoundPlayer:
     def __init__(self, sound_path):
         self.sound_data, self.sample_rate = sf.read(sound_path)
+        self.stream = None
 
     def play(self):
         sd.play(self.sound_data, self.sample_rate)
         sd.wait()
+
+    def play_async(self):
+        thread = threading.Thread(target=self.play)
+        thread.start()
+
+    def is_playing(self):
+        return self.stream is not None
+
+    def stop(self):
+        pass
