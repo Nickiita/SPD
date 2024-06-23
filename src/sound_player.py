dfe55220760442f2a9565fb22d47fc21e51730e0
Nickii <51742504+Nickiita@users.noreply.github.com>
2024-06-23 13:21:06 +1000

    Update sound_player.py

--- src/sound_player.py
+++ src/sound_player.py
@@ -5,13 +5,24 @@ import threading
 
 class SoundPlayer:
     def __init__(self, sound_path, volume=1.0, playback_speed=1.0):
-        self.sound_data, self.sample_rate = sf.read(sound_path)
+        self.sound_path = sound_path
         self.volume = volume
         self.playback_speed = playback_speed
+        self.sound_data, self.sample_rate = self.load_sound(sound_path)
         self.stream = None
         self.sound_data = self.adjust_volume(self.sound_data, self.volume)
         self.sample_rate = int(self.sample_rate * self.playback_speed)
         self.playing = False
+
+    def load_sound(self, sound_path):
+        if sound_path.endswith('.mp3'):
+            import pydub
+            sound = pydub.AudioSegment.from_mp3(sound_path)
+            sound_data = sound.get_array_of_samples()
+            sample_rate = sound.frame_rate
+            return sound_data, sample_rate
+        else:
+            return sf.read(sound_path)
 
     def adjust_volume(self, data, volume):
         return data * volume
@@ -23,7 +34,6 @@ class SoundPlayer:
             self.playing = True
             sd.play(self.sound_data, self.sample_rate)
             sd.wait()
-
         finally:
             if self.stream is not None:
                 self.stream.stop()
