cae71bf341da36254bc1118520d5e04d24700f81
Nickii <51742504+Nickiita@users.noreply.github.com>
2024-06-23 13:20:05 +1000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from soundpad import SoundpadApp
 from database import SoundpadDatabase
+import keyboard
 
 
 def main():
@@ -10,6 +11,13 @@ def main():
     app = QApplication(sys.argv)
     main_window = SoundpadApp(db)
     main_window.show()
+
+    # Инициализация глобального прослушивателя горячих клавиш
+    hotkeys = db.get_hotkeys()
+    for sound_name, hotkey in hotkeys:
+        sound_path = db.get_sound_path_by_name(sound_name)
+        keyboard.add_hotkey(hotkey, lambda path=sound_path: main_window.play_sound_by_path(sound_path))
+
     sys.exit(app.exec_())
 
 
